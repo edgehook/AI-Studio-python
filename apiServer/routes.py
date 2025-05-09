802e4c5753ea7d41b71888d058323490950e0e31
gangqiang01 <474996936@qq.com>
2025-05-09 14:30:24 +0800

    add detect env api to get cudnn&python&vpi&vulkan&tensorrt version

--- apiServer/routes.py
+++ apiServer/routes.py
@@ -1,9 +1,11 @@
 import os
 import random
 import string
+import sys
 from flask import Blueprint, jsonify, request, Flask
 import torch
 from apiServer.detection import detect
+from apiServer.utils import jetson_libraries
 from apiServer.utils import responce
 from flask_cors import CORS
 bp = Blueprint('main', __name__)
@@ -67,24 +69,21 @@ def get_detect_monitor():
     
     monitor_array = []
     if detectIds is not None:
-        try: 
             # detectIds = json.loads(ids)
-            for detectId in detectIds:
-                monitor_map = {}
-                dt = detect.get_detection(detect_id=detectId)
-                if dt is not None:
-                    detect_time = dt.detect_time
-                    if not detect_time:
-                        detect_time = 0
-                    handle_time = dt.handle_time
-                    monitor_map["detectId"] = detectId
-                    monitor_map["detectTime"] = detect_time
-                    monitor_map["handleTime"] = handle_time
-                    monitor_map["name"] = dt.name
-                    monitor_array.append(monitor_map)
-        except json.JSONDecodeError as e:
-            result = responce.result(500, "error", "JSON error")
-            return jsonify(result), 500
+        for detectId in detectIds:
+            monitor_map = {}
+            dt = detect.get_detection(detect_id=detectId)
+            if dt is not None:
+                detect_time = dt.detect_time
+                if not detect_time:
+                    detect_time = 0
+                handle_time = dt.handle_time
+                monitor_map["detectId"] = detectId
+                monitor_map["detectTime"] = round(float(detect_time)*1000, 2)
+                monitor_map["handleTime"] = round(float(handle_time)*1000, 2)
+                monitor_map["fpsTime"]= round(1/float(handle_time), 2)
+                monitor_map["name"] = dt.name
+                monitor_array.append(monitor_map)
     result = responce.result(200, "success",monitor_array)
     return jsonify(result), 200
 @bp.route('/v1/detect/cuda', methods=["GET"])
@@ -97,7 +96,41 @@ def get_cuda_version():
     result = responce.result(200, "success",cuda_version)
     return jsonify(result), 200
 
+@bp.route('/v1/detect/env', methods=["GET"])
+def get_detect_env():
+    cuda_version = ""
+    if torch.cuda.is_available():
+        cuda_version = torch.version.cuda
+    else:
+        print("No available CUDA device was detected。")
+    libs = jetson_libraries.get_libraries()
+    print(f"jetson_libraries, {libs}")
+    tensorrt_version=""
+    if "tensorrt" in libs:
+        tensorrt_version = libs["tensorrt"]
+    cudnn_version=""
+    if "cudnn" in libs:
+        cudnn_version = libs["cudnn"]
+    vpi_version=""
+    if "vpi" in libs:
+        vpi_version = libs["vpi"]
+    vulkan_version=""
+    if "vulkan" in libs:
+        vulkan_version = libs["vulkan"]
+    python_major_version = sys.version_info.major
+    python_minor_version = sys.version_info.minor
 
+    # 组合成 x.y 格式
+    python_version = f"{python_major_version}.{python_minor_version}"
+    env_map = {}
+    env_map["cuda_version"] = cuda_version
+    env_map["cudnn_version"] = cudnn_version
+    env_map["python_version"] = python_version
+    env_map["vpi"] = vpi_version
+    env_map["tensorrt_version"] = tensorrt_version
+    env_map["vulkan_version"]=vulkan_version
+    result = responce.result(200, "success",env_map)
+    return jsonify(result), 200
 @bp.route('/v1/camera/screen', methods=["GET"])
 def get_camera_screen():
     source = request.args.get("source")
